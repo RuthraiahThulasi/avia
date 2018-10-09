aeb83a7f44b97b3ca9b6f06713fb1667e4929a32
ramansah <ramansah@live.com>
2018-10-09 15:35:43 +0530

    Added comments and tests to plug

--- apps/snitch_core/lib/core/tools/multitenancy/plug.ex
+++ apps/snitch_core/lib/core/tools/multitenancy/plug.ex
@@ -1,8 +1,15 @@
 defmodule Snitch.Core.Tools.MultiTenancy.Plug do
+  @moduledoc """
+  Custom plug to parse subdomain and set options in MultiTenancy.Repo
+  Plug will send 404 error if url scheme contains subdomain and the tenant is not present in database.
+  """
+
+  import Plug.Conn
   alias Snitch.Core.Tools.MultiTenancy.Repo
 
   def init(options), do: options
 
+  @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
   def call(%Plug.Conn{host: host} = conn, opts) do
     root_host = Keyword.get(opts, :endpoint).config(:url)[:host]
 
@@ -18,21 +25,25 @@ defmodule Snitch.Core.Tools.MultiTenancy.Plug do
 
   # Private
 
+  @spec root_domain?(String.t(), boolean()) :: boolean()
   defp root_domain?(host, root_host) do
     host in [root_host, "localhost", "0.0.0.0", "127.0.0.1"]
   end
 
+  @spec tenant_exists?(String.t()) :: boolean()
   defp tenant_exists?(host) do
     host
     |> extract_subdomain()
     |> Triplex.exists?()
   end
 
+  @spec extract_subdomain(String.t()) :: String.t()
   defp extract_subdomain(host) do
     String.split(host, ".")
     |> List.first()
   end
 
+  @spec set_repo(boolean(), String.t()) :: boolean()
   defp set_repo(exists, host) do
     if exists,
       do:
@@ -43,6 +54,7 @@ defmodule Snitch.Core.Tools.MultiTenancy.Plug do
     exists
   end
 
+  @spec handle_conn(boolean(), Plug.Conn.t()) :: Plug.Conn.t()
   defp handle_conn(exists, %Plug.Conn{host: host} = conn) do
     reserved_tenant =
       host
@@ -51,7 +63,7 @@ defmodule Snitch.Core.Tools.MultiTenancy.Plug do
 
     if !reserved_tenant and !exists do
       conn
-      |> Plug.Conn.send_resp(:not_found, "Subdomain not found")
+      |> put_status(:not_found)
     else
       conn
     end
